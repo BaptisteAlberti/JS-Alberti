675d4ac8719ab4e123b5b898c6b21b07cd17206a
BaptisteAlberti <135699752+BaptisteAlberti@users.noreply.github.com>
2023-07-22 12:26:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const form = document.getElementById("error-message")
+const form = document.querySelector("form");
+
+const errorMessage = document.querySelector("#error-message");
 
 form.addEventListener("submit", (event) => {
     //Vérifier que les champs du formulaire sont valides.
-
+    verifieChamps();
     //Si les champs du formulaire sont valides, soumettre le formulaire.
     event.preventDefault();
 });
 
 //Si les champs sont vides, renvoyer un message d'erreur. 
-function VerifieChamps() {
-    if (firstName ="") {
-    alert ("Tous les champs doivent être remplis.");
-    return false;
-} else {
-    errorMessage.style.display = "none";
-    return true;
+function verifieChamps() {
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+    const message = document.getElementById("message").value;
+    
+    if (firstName ==="" || lastName==="" || message==="") {
+        errorMessage.style.display = "block";
+    } 
+    else {
+        const div_container = document.createElement("div");
+        const comment_container = document.createElement("div");
+        const user = document.createElement("h3");
+        user.innerText = firstName +" "+ lastName;
+        const message_container = document.createElement("div");
+        const message = document.createElement("p");
+        message.innerText = message;
+
+        const commentList = document.querySelector("#comment-list");
+
+        div_container.appendChild(comment_container);
+        comment_container.appendChild(user);
+        comment_container.appendChild(message_container);
+        message_container.appendChild(message);
+        div_container.className = "flex space-x-4 text-sm text-gray-500";
+        comment_container.className = "flex-1 py-10 ";
+        user.className = "font-medium text-gray-900";
+        message_container.className = "prose prose-sm mt-4 max-w-none text-gray-500";
+        commentList.appendChild(div_container);
+
+        
 }};
+
 
 //Une fois le commentaire correctement créé, il faut l'ajouter à ceux déjà existants.
 
